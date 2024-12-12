b9c74e1dd8a2f1f7bb9a8a0e15515646e92a6f9a
Daniel Krastev <danielkrastev01@gmail.com>
2024-12-12 12:02:14 +0200

    Add routing with page.js

--- src/app.js
+++ src/app.js
@@ -1,3 +1,10 @@
+import page from "./lib/page.js";
+import catsView from "./views/catsView.js";
 import homeView from "./views/homeView.js";
 
-homeView();
+// Setup Routing
+page('/', homeView);
+page('/cats', catsView);
+
+//Start rounting
+page.start();
---
+++ src/lib/page.js
@@ -0,0 +1,5 @@
+import page from "../../node_modules/page/page.mjs";
+
+export default page;
+
+//export {default} from "../../node_modules/page/page.mjs";
